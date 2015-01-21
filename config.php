956a4af11e4a059b66bf9342cb99e4756a83afa0
moshegorin <moshegorin@users.noreply.github.com>
2015-01-20 20:26:55 -0500

    Removed Amazon.com keys
    Removed PUBLIC_KEY, PRIVATE_KEY and ASSOCIATE_TAG
    
    These need to be added to make app work

--- config.php
+++ config.php
@@ -13,14 +13,14 @@ ini_set('max_execution_time',18000); // in seconds
 ini_set('memory_limit','256M');
 ini_set('max_input_time',18000); // in seconds
 
-// Amazon Product API Public Key for CHCycles account
-define('PUBLIC_KEY','AKIAIYICN5TUJRQWFUOQ');  
+// Amazon Product API Public Key for Amazon.com Associates account
+define('PUBLIC_KEY','');  
 
-// Amazon Product API Private Key for CHCycles account
-define('PRIVATE_KEY','gJAp16jOVCs4Yxn/HObCva53A00++ESG5g4T/WCn'); 
+// Amazon Product API Private Key for Amazon.com Associates account
+define('PRIVATE_KEY',''); 
 
-// Amazon Associate Tag for CHCycles Accont
-define('ASSOCIATE_TAG','crowheigcycl-20'); 
+// Amazon Associate Tag for Amazon.com Associates Account
+define('ASSOCIATE_TAG',''); 
 
 // Amazon Region ("com" = commercial)
 define('REGION','com'); 
