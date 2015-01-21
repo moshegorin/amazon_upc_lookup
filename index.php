157ee24f4da8a80736708287438ff0d6defa594f
Moshe Gorin <moshegorin@users.noreply.github.com>
2015-01-20 21:38:52 -0500

    Fixed typos & formatting

--- index.php
+++ index.php
@@ -5,15 +5,11 @@
 	Developed by GorinSystems (www.gorinsystems.com)
 
 	Looks up product information from Amazon.com's database for a spreadsheet of UPC codes
-		
 	Last updated 3/20/2014
 
 **/
 
-// Error reporting on
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
-
-// Set timeout values
 ini_set('max_execution_time',12000);
 ini_set('memory_limit','256M');
 
@@ -29,20 +25,15 @@ if (isset($_GET['route'])){
 	$route = NULL;
 }
 
-// Routing
 switch ($route){
 
-	case "output": // // Process upload and show results
-
-		// Load output controller
+	case "output": // Process upload and show results
 		require("app/controller/output.php");
 		$output = new Output();
 		$output->process($_FILES); // process uploaded files	
 		break;
-
+		
 	default: // Show input form
-
-		// Load input controller
 		require("app/controller/input.php");
 		$input = new Input;
 		$input->show(); // Show input form
